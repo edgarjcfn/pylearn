79fdb0dd06b90ab0520d09788b09c22437f96057
edgarjcfn <edgar.figueiredo@gmail.com>
2014-09-28 13:21:17 +0200

    Initial commit

---
+++ mygame/__init__.js
@@ -0,0 +1,68 @@
+var $builtinmodule = function(name)
+{
+    'use strict';
+
+
+    var Character = function() {
+        this.health = 0;
+        this.x = 0;
+        this.y = 0;
+    };
+
+    Character.prototype.moveForward = function(n) {
+        this.x += n;
+        console.debug(this.x);
+    };
+
+    Character.prototype.jump = function() {
+        
+    };
+
+    Character.prototype.position  = function() {
+        var pos = {'x':this.x, 'y':this.y};
+        return pos;
+    };
+
+
+    /**
+    * SKULPT module definition
+    */
+    var mod = {};
+
+    // 
+    // functions
+    // 
+    mod.alert = new Sk.builtin.func(function(a) {
+        alert(a.v);
+    });
+
+
+    // 
+    // classes
+    // 
+    mod.Character = Sk.misceval.buildClass(mod, function($gbl, $loc) {
+         $loc.__init__ = new Sk.builtin.func(function(self) {
+            self.character = new Character();
+         });
+
+         $loc.moveForward = new Sk.builtin.func(function(self,x) {
+            self.character.moveForward(x.v);
+         });
+         $loc.jump = new Sk.builtin.func(function(self) {
+            self.character.jump();
+         });
+         $loc.position = new Sk.builtin.func(function(self) {
+            var pos = self.character.position();
+
+            return new Sk.builtin.tuple([
+                    Sk.builtin.assk$(pos.x, Sk.builtin.nmber.float$),
+                    Sk.builtin.assk$(pos.y, Sk.builtin.nmber.float$)
+                ]);
+         });
+
+    },
+    'Character', []);
+
+
+    return mod;
+}
