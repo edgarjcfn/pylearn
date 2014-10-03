b188b1726cba0ea02a317f0dc7996b25e5ee0059
edgarjcfn <edgar.figueiredo@gmail.com>
2014-10-03 20:53:36 +0200

    Added isometric tiles

--- pylearn/dev/game/game.js
+++ pylearn/dev/game/game.js
@@ -1,18 +1,44 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameCanvas', { preload: preload, create: create, update: update });
- 
+
 var characterAnimator;
-var ship;
+var isoGroup;
+var mainChar;
 
 function preload() {
-	game.load.image('ship', 'pylearn/dev/game/assets/ship.png');
+	game.load.image('tile', 'pylearn/dev/game/assets/tile.png');
+
+    game.time.advancedTiming = true;
+
+    // Add and enable the plug-in.
+    game.plugins.add(new Phaser.Plugin.Isometric(game));
+
+    // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
+    // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
+    game.iso.anchor.setTo(0.5, 0.2);
 }
- 
+
 function create() {
-	ship = game.add.sprite(350, 300, 'ship');
-	ship.anchor.setTo(0.5, 0.5);
-	characterAnimator = new CharacterAnimator(game, ship);
+    // Create a group for our tiles.
+    isoGroup = game.add.group();
+
+    // Let's make a load of tiles on a grid.
+    spawnTiles();
+
+	characterAnimator = new CharacterAnimator(game, mainChar);
 }
- 
+
 function update() {
 	characterAnimator.update();
+}
+
+function spawnTiles() {
+    var tile;
+    for (var xx = 0; xx < 256; xx += 64) {
+        for (var yy = 0; yy < 256; yy += 64) {
+            // Create a tile using the new game.add.isoSprite factory method at the specified position.
+            // The last parameter is the group you want to add it to (just like game.add.sprite)
+            tile = game.add.isoSprite(xx, yy, 0, 'tile', 0, isoGroup);
+            tile.anchor.set(0.5, 0);
+        }
+    }
 }
